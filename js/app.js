ae54e516fbac986c70270b8be76ded63d636d7e5
Rhian Advincula <rhian.dlra97@gmail.com>
2017-11-13 02:13:49 -0800

    Commented out a JS function

--- js/app.js
+++ js/app.js
@@ -12,34 +12,34 @@ $(document).ready(function() {
   });
 });
 
-// Scrolling magic - http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
-// Borrowed from Paul Lumsdaine
-$(document).ready(function() {
-  console.log("scrolling magic");
-  var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
-
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-
-    if (cur_pos >= 200) {
-      nav.addClass('stick');
-    } else {
-      nav.removeClass('stick');
-    }
-
-    sections.each(function() {
-      var top = $(this).offset().top - nav_height,
-          bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        sections.removeClass('active');
-
-        $(this).addClass('active');
-        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-      }
-    });
-  });
+// // Scrolling magic - http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
+// // Borrowed from Paul Lumsdaine
+// $(document).ready(function() {
+//   console.log("scrolling magic");
+//   var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
+//
+//   $(window).on('scroll', function () {
+//     var cur_pos = $(this).scrollTop();
+//
+//     if (cur_pos >= 200) {
+//       nav.addClass('stick');
+//     } else {
+//       nav.removeClass('stick');
+//     }
+//
+//     sections.each(function() {
+//       var top = $(this).offset().top - nav_height,
+//           bottom = top + $(this).outerHeight();
+//
+//       if (cur_pos >= top && cur_pos <= bottom) {
+//         nav.find('a').removeClass('active');
+//         sections.removeClass('active');
+//
+//         $(this).addClass('active');
+//         nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+//       }
+//     });
+//   });
 
 // In-Page Scroll Animation
 // Borrowed from Paul Lumsdaine
