e44da428604bd920f0c146682ee8a364cb7cca31
Rhian Advincula <rhian.dlra97@gmail.com>
2017-11-13 21:22:07 -0800

    foundations

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-// //Set height of top section to window height - https://j.eremy.net/set-element-height-to-viewport/
-// // Borrowed from Paul Lumsdaine
-// $(document).ready(function() {
-//   function setHeight() {
-//     windowHeight = $(window).innerHeight();
-//     $('section').css('min-height', windowHeight);
-//   };
-//   setHeight();
-//
-//   $(window).resize(function() {
-//     setHeight();
-//   });
-// });
-//
-// Scrolling magic - http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
-// Borrowed from Paul Lumsdaine
+$(document).foundation();
+
+//Set height of top section to window height - https://j.eremy.net/set-element-height-to-viewport/
 $(document).ready(function() {
-  console.log("scrolling magic");
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('section').css('min-height', windowHeight);
+  };
+  setHeight();
+
+  $(window).resize(function() {
+    setHeight();
+  });
+});
+
+// Scrolling magic - http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
+$(document).ready(function() {
   var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
 
   $(window).on('scroll', function () {
@@ -42,7 +41,6 @@ $(document).ready(function() {
   });
 
 // In-Page Scroll Animation
-// Borrowed from Paul Lumsdaine
 $('#top a[href^="#"]').on('click', function(e) {
     nav.find('a').removeClass('active');
     var hash  = this.hash,
@@ -55,10 +53,10 @@ $('#top a[href^="#"]').on('click', function(e) {
 
     if ( hash !== '#top' ) {
         // $hash.velocity('scroll', { duration: 500, offset: -50, complete: addHash }); // Velocity.js
-        $('html').animate({ 'scrollTop': $hash.offset().top -50 }, 500, addHash);
+        $('html,body').animate({ 'scrollTop': $hash.offset().top -50 }, 500, addHash);
     } else {
         // $hash.velocity('scroll', { duration: 500, offset: 0, complete: addHash }); // Velocity.js
-        $('html').animate({ 'scrollTop': $hash.offset().top }, 500, addHash);
+        $('html,body').animate({ 'scrollTop': $hash.offset().top }, 500, addHash);
     }
     e.preventDefault();
   });
@@ -68,42 +66,3 @@ $('#top a[href^="#"]').on('click', function(e) {
   $("a[href^='https://']").attr("target","_blank");
 
 });
-// Scroll to specific values
-// scrollTo is the same
-// Select all links with hashes
-// $('a[href*="#"]')
-//   // Remove links that don't actually link to anything
-//   .not('[href="#"]')
-//   .not('[href="#0"]')
-//   .click(function(event) {
-//     // On-page links
-//     if (
-//       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-//       &&
-//       location.hostname == this.hostname
-//     ) {
-//       console.log("Inside if");
-//       // Figure out element to scroll to
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       // Does a scroll target exist?
-//       if (target.length) {
-//         // Only prevent default if animation is actually gonna happen
-//         event.preventDefault();
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000, function() {
-//           // Callback after animation
-//           // Must change focus!
-//           var $target = $(target);
-//           $target.focus();
-//           if ($target.is(":focus")) { // Checking if the target was focused
-//             return false;
-//           } else {
-//             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//             $target.focus(); // Set focus again
-//           };
-//         });
-//       }
-//     }
-//   });
