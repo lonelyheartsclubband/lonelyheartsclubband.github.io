854f34c9f7b37d26949018f1f2359984610dd409
Rhian Advincula <rhian.dlra97@gmail.com>
2020-03-26 20:42:34 -0700

    March 2020 Update
    Redid Design on Index

--- js/new.js
+++ js/new.js
@@ -77,3 +77,5 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+    
